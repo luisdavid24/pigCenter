c24eefbf466492ff749fe8ea836cf97a578f00a0
Luis David <david12.29@hotmail.com>
2024-03-07 17:57:54 -0500

    Update, the Feeding class will be finished

--- src/main/java/com/Farm/pigCenter/model/Feeding.java
+++ src/main/java/com/Farm/pigCenter/model/Feeding.java
@@ -1,4 +1,29 @@
 package com.Farm.pigCenter.model;
 
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+@Entity
+@Table(name = "Porcine")
 public class Feeding {
+    @Id
+    @Column(name = "id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @Column(name = "description")
+    private String description;
+
+    @Column(name = "dose")
+    private float dose;
+    
+
 }
