6f43b746875b2512329824e61c489ca7ba14479b
Luis David <david12.29@hotmail.com>
2024-03-07 16:55:58 -0500

    Update, the cliente will  be done

--- src/main/java/com/Farm/pigCenter/model/Client.java
+++ src/main/java/com/Farm/pigCenter/model/Client.java
@@ -1,6 +1,60 @@
 package com.Farm.pigCenter.model;
 
 public class Client {
+        private int id;
+        private String name;
 
-    
+        private String lastName;
+
+        private String adress;
+
+        private String phone;
+
+        public Client(int id, String name, String lastName, String adress, String phone) {
+            this.id = id;
+            this.name = name;
+            this.lastName = lastName;
+            this.adress = adress;
+            this.phone = phone;
+        }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getAdress() {
+        return adress;
+    }
+
+    public void setAdress(String adress) {
+        this.adress = adress;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
 }
