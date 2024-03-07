ae0fb2588328dd1a02ea67786a0282daede9d23e
Luis David <david12.29@hotmail.com>
2024-03-07 16:50:45 -0500

    this is the first commmit

---
+++ src/main/java/com/Farm/pigCenter/PigCenterApplication.java
@@ -0,0 +1,13 @@
+package com.Farm.pigCenter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PigCenterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PigCenterApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/Farm/pigCenter/PigCenterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Farm.pigCenter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PigCenterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
