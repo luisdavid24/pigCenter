cde23f589d3e1f1b76704c2b62acb37f6659d97d
Luis David <david12.29@hotmail.com>
2024-03-18 00:31:41 -0500

    Update, the porcino has created a new element

--- src/main/resources/static/js/porcinos.js
+++ src/main/resources/static/js/porcinos.js
@@ -11,18 +11,58 @@ async function cargarUsuarios() {
            
         });
         const data = await request.json();
-        console.log(data);
+        //console.log(data);
 
 
         let listadoHtml = '';
         for (let porcino of data) {
-            console.log(porcino);
             let porcinoRow='<tr><td>'+porcino.id+'</td><td>' + porcino.race + '</td><td>'+porcino.age + '</td><td>' +porcino.weight + '</td><td>'
                     + porcino.client+'</td><td>Pendiente</td></tr>';
                     listadoHtml+=porcinoRow;
         }
 
        document.querySelector('#tablePorcino tbody').outerHTML = listadoHtml;
+    } catch (error) {
+        console.error('Error:', error);
+    }
+}
+
+    
+
+
+async function crearPorcino() {
+    try {
+
+        let nuevoPorcino = {};
+        nuevoPorcino.race = document.getElementById('opcionesPorcino').value;
+        nuevoPorcino.age = document.getElementById('textEdadPorcino').value;
+        nuevoPorcino.weight = document.getElementById('textPesoPorcino').value;
+      
+        // const nuevoPorcino = {
+        //     race: 'Raza del porcino',
+        //     age: 1, // Edad del porcino
+        //     weight: 100, // Peso del porcino
+        //     client: 'Cliente' // Nombre del cliente
+        // };
+
+        const request = await fetch('http://localhost:8080/porcino', {
+            method: 'POST',
+            headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(nuevoPorcino)
+        });
+
+        if (request.ok) {
+            console.log('Porcino creado exitosamente');
+            console.log(nuevoPorcino);
+
+            // Llamar a la función cargarUsuarios después de crear el porcino
+            cargarUsuarios();
+        } else {
+            throw new Error('Error al crear el porcino');
+        }
     } catch (error) {
         console.error('Error:', error);
     }
