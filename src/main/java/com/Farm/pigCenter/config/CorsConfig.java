2fbdf2fe1b8ed03486f905d2494324cfda21a4df
Luis David <david12.29@hotmail.com>
2024-03-17 23:58:28 -0500

    Update, the CORS is working

--- src/main/java/com/Farm/pigCenter/config/CorsConfig.java
+++ src/main/java/com/Farm/pigCenter/config/CorsConfig.java
@@ -1,4 +1,25 @@
 package com.Farm.pigCenter.config;
 
-public class CorsConfig {
+import org.apache.catalina.filters.CorsFilter;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+
+
+@Configuration
+public class CorsConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("http://127.0.0.1:5500") // Reemplaza con la URL de tu frontend
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                .allowedHeaders("*")
+                .allowCredentials(true)
+                .maxAge(3600);
+    }
 }
