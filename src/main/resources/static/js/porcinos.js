0076536a2a4020842eef4d347a6b6fc259df5946
Luis David <david12.29@hotmail.com>
2024-03-19 00:32:20 -0500

    Update

--- src/main/resources/static/js/porcinos.js
+++ src/main/resources/static/js/porcinos.js
@@ -1,4 +1,9 @@
 cargarUsuarios();
+document.addEventListener("DOMContentLoaded", function() {
+    generarReportePorcino();
+
+});
+
 
 async function cargarUsuarios() {
     try {
@@ -37,14 +42,8 @@ async function crearPorcino() {
         nuevoPorcino.race = document.getElementById('opcionesPorcino').value;
         nuevoPorcino.age = document.getElementById('textEdadPorcino').value;
         nuevoPorcino.weight = document.getElementById('textPesoPorcino').value;
-      
-        // const nuevoPorcino = {
-        //     race: 'Raza del porcino',
-        //     age: 1, // Edad del porcino
-        //     weight: 100, // Peso del porcino
-        //     client: 'Cliente' // Nombre del cliente
-        // };
-
+       
+        
         const request = await fetch('http://localhost:8080/porcino', {
             method: 'POST',
             headers: {
@@ -57,8 +56,10 @@ async function crearPorcino() {
         if (request.ok) {
             console.log('Porcino creado exitosamente');
             console.log(nuevoPorcino);
+            let boton = document.getElementById("btnClose");
+            boton.click();
+            
 
-            // Llamar a la función cargarUsuarios después de crear el porcino
             cargarUsuarios();
         } else {
             throw new Error('Error al crear el porcino');
@@ -66,7 +67,77 @@ async function crearPorcino() {
     } catch (error) {
         console.error('Error:', error);
     }
+    
+ 
 }
 
 
-     
+
+async function generarReportePorcino() {
+    try {
+        const request = await fetch('http://localhost:8080/porcino', {
+            method: 'GET',
+            headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json'
+            }
+           
+        });
+        const data = await request.json();
+        let cantidadYork=0;
+        let cantidadHamp=0;
+        let cantidadDuroc=0;
+
+        for (let porcino of data) {
+            if(porcino.race=="york"){
+            cantidadYork++;
+           }
+           if(porcino.race=="hamp"){
+            cantidadHamp++;
+           }
+           if(porcino.race=="duroc"){
+            cantidadDuroc++;
+           }
+        }
+        document.querySelector('#yorkTd').outerHTML ='<td>'+cantidadYork+'</td>';
+        document.querySelector('#hampTd').outerHTML ='<td>'+cantidadHamp+'</td>';
+        document.querySelector('#durocTd').outerHTML ='<td>'+cantidadDuroc+'</td>';
+
+        let dataVector={};
+        dataVector[0]=cantidadYork;
+        dataVector[1]=cantidadHamp;
+        dataVector[2]=cantidadDuroc;
+
+
+        var ctx = document.getElementById('torta').getContext('2d');
+        var myPieChart = new Chart(ctx, {
+        type: 'pie',
+        data: {
+        labels: ['york', 'hamp', 'duroc'],
+        datasets: [{
+            label: '# of Votes',
+            data: dataVector,
+            backgroundColor: [
+            'rgba(128, 0, 128, 1)',
+            'rgba(127, 255, 212, 1)',
+            'rgba(180, 144, 202, 1)'
+            ],
+            borderColor: [
+            'rgba(0, 0, 255, 1)',
+            'rgba(0, 0, 255, 1)',
+            'rgba(0, 0, 255, 1)'
+            ],
+            borderWidth: 1
+        }]
+        },
+        options: {
+        // Aquí puedes personalizar opciones adicionales, como la leyenda, el título, etc.
+        }
+        });
+        
+    } catch (error) {
+        console.error('Error:', error);
+    }
+}
+
+
